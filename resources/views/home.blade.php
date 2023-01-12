0ab4456f1301167dbbd4909b5401f325e96a2b80
a.alampi <a.alampi@my-groom-service.com>
2023-01-12 18:46:17 +0100

    Graph charts in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('base')
 
 @section('content')
+<h1>Bonjour le titre</h1>
+<h2>bonjour</h2>
+<h3>bonjour</h3>
 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium error qui, officiis impedit sapiente blanditiis, perspiciatis magnam accusamus suscipit ratione natus sequi, molestias a itaque.</p>
 @endsection
--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -1,8 +1,34 @@
 <header>
     <nav>
-        <a href="#" class="nav-icon" aria-label="visit homagepage" aria-current="page"><img src="/public/assets/img/logo-           
-        manualLibs.jpg" alt="">ManualsLib</a>
+        <a href="#" class="nav-icon" aria-label="visit homepage" aria-current="page">
+            <img src="{{asset('assets/img/logo-manualsLib-rogn.jpg')}}" alt="">
+        </a>
 
+        <div class="main-navlinks">
+            <button class="hamburger" type="button">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <div class="navlinks-container">
+                <a href="#" aria-current="page"></a>
+                <a href="#">Manuals</a>
+                <a href="#">Features</a>
+                <a href="#">Brands</a>
+                <a href="#">Pricing</a>
+                <a href="#">Api</a>
+                <a href="#">Press</a>
+                <a href="#">About</a>
+            </div>
+        </div>
+            <div class="nav-authentication">
+                <a href="#" class="sign-user" aria-label="Sign in page">
+                    <img src=" {{asset('assets/img/user-avatar-filled.svg')}} " alt="user-icon">
+                </a>
+                <div class="signs-btn">
+                <button type="button">Sign in / Sign up</button>
+            </div>
+        </div>
     </nav>
 </header>
 
