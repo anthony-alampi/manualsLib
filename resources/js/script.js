53d3a5f17b781ab35f0ab23d39771812dc3ff9dd
a.alampi <a.alampi@my-groom-service.com>
2023-01-13 20:04:32 +0100

    Homepage completed

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,8 +13,6 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
-
-
 //--------------------------------------Hamburger-------------------------------------//
 const hamburgerToggler = document.querySelector(".hamburger");
 const navLinksContainer = document.querySelector(".navlinks-container");
@@ -34,7 +32,44 @@ new ResizeObserver(entries => {
         navLinksContainer.style.transition = "none";
     }
 }).observe(document.body)
-//--------------------------------------End Hamburger-------------------------------------//
 
+//--------------------Reveal Observer--------------------------//
+const ratio = 0.1;
+const option = {
+    root: null, //element racine
+    rootMargin: "0px", // depasse cette marge pour etre visible
+    threshold: 0.1, // a quel moment l'élément doit etre detecter  ici 10%
+};
 
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add("reveal-visible");
+            observer.unobserve(entry.target);
+        } else {
+            console.log("invisible");
+        }
+    });
+};
+const observer = new IntersectionObserver(handleIntersect, option);
+document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
+    observer.observe(r);
+});
+//--------------------Customers Feeadbacks-------------------------//
+let slide = document.getElementById('slide');
+let upArrow = document.getElementById('upArrow');
+let downArrow = document.getElementById('downArrow');
+let x = 0;
 
+upArrow.addEventListener("click", () => {
+    if( x > "-900"){
+        x = x - 300;
+        slide.style.top = x + "px";
+    }
+});
+downArrow.addEventListener("click", () => {
+    if( x < 0 ){
+        x = x + 300;
+        slide.style.top = x + "px";
+    }
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,4 @@
 @extends('base')
-
 @section('content')
 
 <header class="home-header">
@@ -16,6 +15,10 @@
     </div>
     <div class="illustration">
         <img src="{{asset('assets/img/ils-01.png')}}" alt="">
+        <img src="{{asset('assets/img/smartphone.svg')}}" class="floating-image floating-image1" />
+        <img src="{{asset('assets/img/ipad.svg')}}" class="floating-image floating-image2" />
+        <img src="{{asset('assets/img/watch-tv.svg')}}" class="floating-image floating-image3" />
+        <img src="{{asset('assets/img/electric-appliance.svg')}}" class="floating-image floating-image4" />
     </div>
 </header>
 <main>
@@ -23,19 +26,54 @@
         <div class="title-information">
             <h2>{{ trans('lang.STR_TITLE_FORM_3') }}</h2>
             <h2>{{ trans('lang.STR_TITLE_FORM_4') }}</h2>
-            <div class="brands-container">
-                <img src="{{asset('assets/img/whirlpool.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Sony-Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Samsung_Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Panasonic-Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Logo-CANDY-Simple-3.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Liebherr-Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/HIGH-ONE-Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/electronics-lg.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Electrolux-Logo.png')}}" alt="" class="brand">
-                <img src="{{asset('assets/img/Bosch-logo.png')}}" alt="" class="brand">
+            <div class="slider">
+	            <div class="slide-track">
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Bosch-logo.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Electrolux-Logo.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/electronics-lg.png')}}"  alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/HIGH-ONE-Logo.png')}}"  alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Liebherr-Logo.png')}}"  alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Logo-CANDY-Simple-3.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Panasonic-Logo.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Samsung_Logo.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/Sony-Logo.png')}}" alt="Brands logo" />
+		            </div>
+		            <div class="slide">
+			            <img src="{{asset('assets/img/whirlpool.png')}}" alt="Brands logo" />
+		            </div>
+                    <div class="slide">
+			            <img src="{{asset('assets/img/whirlpool.png')}}" alt="Brands logo" />
+		            </div>
+                    <div class="slide">
+			            <img src="{{asset('assets/img/whirlpool.png')}}" alt="Brands logo" />
+		            </div>
+                    div class="slide">
+			            <img src="{{asset('assets/img/whirlpool.png')}}" alt="Brands logo" />
+		            </div>
+                    <div class="slide">
+			            <img src="{{asset('assets/img/whirlpool.png')}}" alt="Brands logo" />
+		            </div>
+	            </div>
             </div>
         </div>
+
         <section class="devices-cover">
             <article>
                 <h2>{{ trans('lang.STR_TITLE_FORM_5') }}</h2>
@@ -43,36 +81,92 @@
                     <div class="device-card">
                         <img src="{{asset('assets/img/electric-appliance.svg')}}" alt="" class="device">
                     </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/computer.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/smartphone.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/ipad.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/document.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/watch-tv.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/wifi.svg')}}" alt="" class="device">
-                        </div>
-                        <div class="device-card">
-                            <img src="{{asset('assets/img/amplifier.svg')}}" alt="" class="device">
-                        </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/computer.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/smartphone.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/ipad.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/document.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/watch-tv.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/wifi.svg')}}" alt="" class="device">
+                    </div>
+                    <div class="device-card">
+                        <img src="{{asset('assets/img/amplifier.svg')}}" alt="" class="device">
+                    </div>
                 </div>
             </article>
-            <article class="title-text_1">
+
+            <article class="title-text reveal-1">
                 <div class="text-container">
-                    <h2 class="left-position">{{ trans('lang.STR_TITLE_1') }}</h2>
+                    <h2>{{ trans('lang.STR_TITLE_1') }}</h2>
                     <p>{{ trans('lang.STR_TEXT_1') }}</p>
                 </div>
                 <img src="{{asset('assets/img/user-guide.jpg')}}" alt="">
             </article>
+
+            <article class="title-text reveal-1">
+                <img src="{{asset('assets/img/user-guide.jpg')}}" alt="">
+                <div class="text-container">
+                    <h2>{{ trans('lang.STR_TITLE_2') }}</h2>
+                    <p>{{ trans('lang.STR_TEXT_2') }}</p>
+                </div>
+            </article>
+
+            <article class="title-text reveal-1">
+                <div class="text-container">
+                    <h2>{{ trans('lang.STR_TITLE_3') }}</h2>
+                    <p>{{ trans('lang.STR_TEXT_3') }}</p>
+                </div>
+                <img src="{{asset('assets/img/user-guide.jpg')}}" alt="">
+            </article>
+
+            <div class="hero reveal-1">
+                <h2 class='testimonial-title'>{{ trans('lang.STR_TITLE_4') }}</h2>
+                <div class="review-box">
+                    <div class="" id="slide">
+                        <div class="card">
+                            <div class="profile">
+                                <img src="{{asset('assets/img/stars.svg')}}" alt="">
+                            </div>
+                            <p>{{ trans('lang.STR_4') }}</p>
+                        </div>
+                        <div class="card">
+                            <div class="profile">
+                                <img src="{{asset('assets/img/stars.svg')}}" alt="">
+                            </div>
+                            <p>{{ trans('lang.STR_5') }}</p>
+                        </div>
+                        <div class="card">
+                            <div class="profile">
+                                <img src="{{asset('assets/img/stars.svg')}}" alt="">
+                            </div>
+                            <p>{{ trans('lang.STR_6') }}</p>
+                        </div>
+                        <div class="card">
+                            <div class="profile">
+                                <img src="{{asset('assets/img/stars.svg')}}" alt="">
+                            </div>
+                            <p>{{ trans('lang.STR_4') }}</p>
+                        </div>
+                    </div>
+                    
+                    <div class="sidebar">
+                        <img class="up" id="upArrow" src="{{asset('assets/img/up-arrow.svg')}}" alt="">
+                        <img class="down" id="downArrow" src="{{asset('assets/img/up-arrow.svg')}}" alt="">
+                    </div>
+                </div>
+                </div>
+
+            <div>
         </section>
     </article>
 </main>
