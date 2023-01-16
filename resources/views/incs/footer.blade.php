61d017f63a54a9739aa45c3c72ff979f86f9dd16
a.alampi <a.alampi@my-groom-service.com>
2023-01-16 11:58:39 +0100

    Footer created(front)

---
+++ resources/views/incs/footer.blade.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="link-footer-container">
+        <img src="{{asset('assets/img/logo 3 Vector file.svg')}}" alt="">
+        <div class="links-container">
+            <a href="#" class="link-item">Home</a>
+            <a href="#" class="link-item">Press</a>
+            <a href="#" class="link-item">Pricing</a>
+            <a href="#" class="link-item">About</a>
+        </div>
+        <div class="links-container">
+            <a href="#" class="link-item">Features</a>
+            <a href="#" class="link-item">Manuals</a>
+            <a href="#" class="link-item">Api</a>
+            <a href="#" class="link-item">Faq</a>
+        </div>
+        <div class="links-container">
+            <a href="#" class="link-item">Subscription</a>
+            <a href="#" class="link-item">Cancellation</a>
+            <a href="#" class="link-item">Cookies</a>
+            <a href="#" class="link-item">Terms</a>
+            <a href="#" class="link-item">Privacy</a>
+        </div>
+        <div class="links-container">
+            <a href="#" class="link-item">Carreers</a>
+            <a href="#" class="link-item">Contact</a>
+        </div>
+            <div class="links-container">
+        <img src="{{asset('assets/img/paypal.svg')}}" alt="Paypal payment">
+        <img src="{{asset('assets/img/stripe.svg')}}" alt="Stripe payment">
+    </div>
+    </div>
+
+    <p class="copyright">Copyright 2023 - <a href="#">ManualsLib</a></p>
+</footer>
