04e71475264227fd5f0849d4c1ebcce3c961b1cb
a.alampi <a.alampi@my-groom-service.com>
2023-01-17 02:07:04 +0100

    Completed & Repsonsive Faq page

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -9,43 +9,43 @@
     <article>
         <section>
             <article class="faq">
-                <div class="faq-question">
+                <div class="faq-question reveal-1">
                     <h2>{{trans('lang.STR_TITLE_43')}}</h2>
                     <p>{{trans('lang.STR_TEXT_31')}}</p>
                 </div>
-                <div class="faq-question faq-custom-bg">
+                <div class="faq-question faq-custom-bg reveal-1">
                     <h2>{{trans('lang.STR_TITLE_44')}}</h2>
                     <p>{{trans('lang.STR_TEXT_32')}}</p>
                 </div>
-                <div class="faq-question">
+                <div class="faq-question reveal-1">
                     <h2>{{trans('lang.STR_TITLE_45')}}</h2>
                     <p>{{trans('lang.STR_TEXT_33')}}</p>
                 </div>
-                <div class="faq-question faq-custom-bg">
+                <div class="faq-question faq-custom-bg reveal-1">
                     <h2>{{trans('lang.STR_TITLE_46')}}</h2>
                     <p>{{trans('lang.STR_TEXT_34')}}</p>
                 </div>
-                <div class="faq-question">
+                <div class="faq-question reveal-1">
                     <h2>{{trans('lang.STR_TITLE_47')}}</h2>
                     <p>{{trans('lang.STR_TEXT_35')}}</p>
                 </div>
-                <div class="faq-question faq-custom-bg">
+                <div class="faq-question faq-custom-bg reveal-1">
                     <h2>{{trans('lang.STR_TITLE_48')}}</h2>
                     <p>{{trans('lang.STR_TEXT_36')}}</p>
                 </div>
-                <div class="faq-question">
+                <div class="faq-question reveal-1">
                     <h2>{{trans('lang.STR_TITLE_49')}}</h2>
                     <p>{{trans('lang.STR_TEXT_37')}}</p>
                 </div>
-                <div class="faq-question faq-custom-bg">
+                <div class="faq-question faq-custom-bg reveal-1">
                     <h2>{{trans('lang.STR_TITLE_50')}}</h2>
                     <p>{{trans('lang.STR_TEXT_38')}}</p>
                 </div>
-                <div class="faq-question">
+                <div class="faq-question reveal-1">
                     <h2>{{trans('lang.STR_TITLE_51')}}</h2>
                     <p>{{trans('lang.STR_TEXT_39')}}</p>
                 </div>
-                <div class="faq-question faq-custom-bg">
+                <div class="faq-question faq-custom-bg reveal-1">
                     <h2>{{trans('lang.STR_TITLE_52')}}</h2>
                     <p>{{trans('lang.STR_TEXT_40')}}</p>
                 </div>
