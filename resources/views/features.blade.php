83e4163496919342d979ed9e521187c4e33d550e
a.alampi <a.alampi@my-groom-service.com>
2023-01-21 03:48:07 +0100

    Responsive feature in progress

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -2,15 +2,21 @@
 
 @section('content')
 
-<header class="custom-page-header">
+<header class="features-header">
     <h1>{{ trans('lang.STR_TITLE_12/5') }}</h1>
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item1" />
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item2" />
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item3" />
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item4" />
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item5" />
+    <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item6" />
 </header>
 <main>
     <article>
         <section>
 
-            <article class="features-container reveal-1">
-                <img class="resize-svg" src="{{ asset('assets/img/user-guide.jpg') }}" alt="">
+            <article class="features-container1 reveal-1">
+                <img class="illustration" src="{{ asset('assets/img/need-user-manual.jpg') }}" alt="">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_12') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_9') }} </p>
@@ -36,44 +42,68 @@
                 </div>
             </article>
 
-            <article class="features-container reveal-1">
+            <article class="features-container1 bg-block reveal-1">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_16') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_13') }} </p>
                 </div>
-                <img class="svg" src="{{ asset('assets/img/is-more-manuals.svg') }}" alt="">
+                <img src="{{ asset('assets/img/more-product.png') }}" class="image-block" alt="">
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item1" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item2" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item3" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item4" />
+                <img src="{{asset('assets/imgs/circle-effect-1.svg')}}" class="circle-item" />
             </article>
 
-            <article class="features-container reveal-1">
-                <img src="{{ asset('assets/img/more-product.png') }}" alt="">
+            <article class="features-container2 reveal-1">
+                <img src="{{ asset('assets/img/electro.jpeg') }}" class="image-block" alt="">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_17') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_14') }} </p>
                 </div>
+                <img src="{{asset('assets/img/red-flock.svg')}}" class="flock-item1" />
+                <img src="{{asset('assets/img/red-flock.svg')}}" class="flock-item2" />
+                <img src="{{asset('assets/imgs/triangle-effect-1.svg')}}" class="triangle-item" />
             </article>
 
-                <article class="features-container reveal-1">
+                <article class="features-container3 bg-block2 reveal-1">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_18') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_15') }} </p>
                 </div>
-                <img class="resize-svg" src="{{ asset('assets/img/more-translate.png') }}" alt="">
+                <img class="image-block" src="{{ asset('assets/img/language-docs.jpeg') }}" alt="">
+                <img src="{{asset('assets/imgs/flock-1.svg')}}" class="flock-item1" />
+                <img src="{{asset('assets/imgs/flock-3.svg')}}" class="flock-item2" />
+                <img src="{{asset('assets/imgs/circle-effect-1.svg')}}" class="circle-item" />
             </article>
 
-            <article class="features-container reveal-1">
-                <img src="{{ asset('assets/img/more-calendar.png') }}" alt="">
+            <article class="features-container4 reveal-1">
+                <img src="{{ asset('assets/img/maintenance-calendar.svg') }}" alt="">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_19') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_16') }} </p>
                 </div>
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item1" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item2" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item3" />
+                <img src="{{asset('assets/imgs/triangle-effect-1.svg')}}" class="triangle-item" />
             </article>
-                <article class="features-container reveal-1">
+
+                <article class="features-container5 reveal-1">
                 <div class="features-block">
                     <h2>{{ trans('lang.STR_TITLE_21/1') }}</h2>
                     <p> {{ trans('lang.STR_TEXT_17') }} </p>
                 </div>
-                <img class="resize-svg" src="{{ asset('assets/img/more-assist.png') }}" alt="">
+                <img class="image-block" src="{{ asset('assets/img/more-assist.png') }}" alt="">
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item1" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item2" />
+                <img src="{{asset('assets/imgs/carre.svg')}}" class="square-item3" />
+                <img src="{{asset('assets/imgs/backslash.svg')}}" class="backslash-item" />
+                <img src="{{asset('assets/imgs/more.svg')}}" class="more-item1" />
+                <img src="{{asset('assets/imgs/more.svg')}}" class="more-item2" />
+                <img src="{{asset('assets/imgs/circle-effect-1.svg')}}" class="circle-item" />
             </article>
+
         </section>
     </article>
 </main>
